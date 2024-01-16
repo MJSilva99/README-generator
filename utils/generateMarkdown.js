a31f50bcfa9f028996ad51feb969ade2b345e6a5
Michael <michaelj.silva@hotmail.com>
2024-01-16 22:29:14 +0000

    Added code to generate readme content from answers provided

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,34 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  This project is licensed under the ${data.license} license.
+  
+  ## Contributors
+  ${data.contributors}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  For any questions, please contact [${data.githubUsername}](https://github.com/${data.githubUsername}) or email at ${data.email}.   
 `;
 }
 
